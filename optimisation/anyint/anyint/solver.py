449fb17f28ff797523f9ec2da07d240afe1725e4
zhi ming <zhiming@PropertyOfLuke>
2023-06-10 23:04:58 +0800

    >add: discrete optimisation wk 1 assignment

--- optimisation/anyint/anyint/solver.py
+++ optimisation/anyint/anyint/solver.py
@@ -4,8 +4,7 @@ import random
 
 def solve_it(input_data):
     # return a positive integer, as a string
-    random_int = random.randint(-10, 10)
-    return str(random_int)
+    return str(10)
 
 if __name__ == '__main__':
     print('This script submits the integer: %s\n' % solve_it(''))
